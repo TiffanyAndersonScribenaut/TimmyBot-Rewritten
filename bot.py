5b62503f90e69819125f7f63985537e171fd145e
TimmyTimothyAnderson <43443858+TiffanyAndersonScribenaut@users.noreply.github.com>
2019-02-09 13:51:42 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+
+bot = commands.Bot(command_prefix="t!")
+
+@bot.event
+async def on_ready():
+   print("Ready!")
+
+@bot.command
+async def ping(ctx):
+   await bot.say("Pong!")
+
+bot.run(os.getenv.TOKEN)
