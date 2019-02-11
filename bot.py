e532835c94f4b2048611dd94b228df2ba35b27b6
TimmyTimothyAnderson <43443858+TiffanyAndersonScribenaut@users.noreply.github.com>
2019-02-11 10:33:32 +0800

    Fixed bug where a command broke the bot.

--- bot.py
+++ bot.py
@@ -17,10 +17,10 @@ async def ping(ctx):
 @bot.command(pass_context=True)
 async def botinfo(ctx):
     embed=discord.Embed(title="Info", description="Sup. My name is TimmyBot. ", color=0xebc000)
-embed.add_field(name=Name, value=TimmyBot, inline=False)
-embed.add_field(name=Bot Author, value=TimmyTimothyAnderson#3719, inline=False)
-embed.add_field(name=Library, value=discord.py (Python), inline=False)
-embed.set_footer(text="Use t!help to get a list of commands")
-await self.bot.say(embed=embed)
+    embed.add_field(name=Name, value=TimmyBot, inline=False)
+    embed.add_field(name=Bot Author, value=TimmyTimothyAnderson#3719, inline=False)
+    embed.add_field(name=Library, value=discord.py (Python), inline=False)
+    embed.set_footer(text="Use t!help to get a list of commands")
+    await self.bot.say(embed=embed)
 
 bot.run(os.getenv('TOKEN'))
